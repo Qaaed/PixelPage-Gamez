c4c46304e96675c67ad560b910a850f9191afae0
Qaaed Usaim <mohamedqxxed@gmail.com>
2025-08-17 20:18:40 +0530

    Updated The Board to handle user inputs

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,28 +1,140 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+
 import "./TicTacToe.css";
 import circle_icon from "../../assets/circle.png";
 import cross_icon from "../../assets/cross.png";
+
+let data = ["", "", "", "", "", "", "", "", ""]; //stores the tic tac toe user inputs
+
 const TicTacToe = () => {
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState(false);
+  let titleRef = useRef(null);
+
+  const toggle = (e, num) => {
+    if (lock) {
+      return 0;
+    }
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src='${cross_icon}'>`;
+      data[num] = "x";
+      setCount(++count);
+    } else {
+      e.target.innerHTML = `<img src='${circle_icon}'>`;
+      data[num] = "o";
+      setCount(++count);
+    }
+    checkWin();
+  };
+
+  const checkWin = () => {
+    // Rows
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data[2]);
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      won(data[5]);
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      won(data[8]);
+    }
+    // Columns
+    else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      won(data[6]);
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      won(data[7]);
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      won(data[8]);
+    }
+    // Diagonals
+    else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      won(data[8]);
+    } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
+      won(data[6]);
+    }
+    // Draw check
+    else if (!data.includes("")) {
+      won("draw");
+    }
+  };
+
+  const won = (winner) => {
+    setLock(true);
+    if (winner === "x") {
+      titleRef.current.innerHTML = `<img src="${cross_icon}" class="winner-icon"> has won the game.`;
+      titleRef.current.classList.add("winner-message");
+    } else if (winner === "o") {
+      titleRef.current.innerHTML = `<img src="${circle_icon}" class="winner-icon"> has won the game.`;
+      titleRef.current.classList.add("winner-message");
+    } else {
+      titleRef.current.innerHTML = `You have Drawed!`;
+      titleRef.current.classList.add("winner-message");
+    }
+  };
   return (
     <div className="container">
-      <h1 className="title">
+      <h1 className="title" ref={titleRef}>
         Tic Tac Toe Game Built Using <span>React</span>
       </h1>
+      <span id="winner_loser"></span>
       <div className="board">
         <div className="row1">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 0);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 1);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 2);
+            }}
+          ></div>
         </div>
         <div className="row2">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 3);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 4);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 5);
+            }}
+          ></div>
         </div>
         <div className="row3">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 6);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 7);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 8);
+            }}
+          ></div>
         </div>
       </div>
       <button className="reset">Reset</button>
